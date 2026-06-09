9322b76395ee056bb42d6b4d0add9f2d30e44903
Tonmoy Paul <snahasishpaul7@gmail.com>
2026-06-09 22:52:21 +0600

    draw line in image using open cv

---
+++ drawimageline.py
@@ -0,0 +1,38 @@
+import cv2
+
+image = cv2.imread("beautiful-anime-character-cartoon-scene.jpg") #load image
+
+
+if image is  not None:
+    
+    img2 = cv2.resize(image,(800,600)) #resize or reshape the picture
+
+    
+    print("image loaded")
+    
+    pt1 = 400,0
+    
+    pt2 = 400,600
+    
+    color = (0,0,255)
+    
+    thickness = 5
+    
+    pt3 = 0,300
+    
+    pt4 = 800,300
+    
+    cv2.line(img2,pt3,pt4,color,thickness)
+    
+    cv2.line(img2,pt1,pt2,color,thickness)
+    
+    cv2.imshow("This is your image..." ,img2) #opening image 
+    
+    cv2.waitKey(0) #The image window remains open until the user clicks a button.
+    
+    cv2.destroyAllWindows() #for closing the image window
+else:
+    
+    print("There is a problem.....")
+    
+
